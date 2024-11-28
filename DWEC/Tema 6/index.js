e56d17d1b37321f86e9e8b167345fcf2dffb64e5
Daniel Trivino <danieltrivinomarin@gmail.com>
2024-11-28 10:09:10 +0100

    ayax peliculas reformado 1

--- DWEC/Tema 6/index.js
+++ DWEC/Tema 6/index.js
@@ -1,19 +1,17 @@
 //AJAX:
 
-var pelicaBusqueda = "";
+var peliculaBusqueda = "";
 var paginaBuscar = 2;
 
 window.onload = ()=>{
     document.getElementById("btn").addEventListener("click", peticionAJAXModerna);
     document.getElementById("btn1").addEventListener("click", cargarPaginas);
-
 }
 
 function peticionAJAX () {
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
-        //Llega la respuesta y ademas ha encontrado lo que he pedido
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("index").innerHTML = this.responseText;
         }
@@ -24,47 +22,86 @@ function peticionAJAX () {
 }
 
 function peticionAJAXModerna() {
-    let pelicaBusqueda = document.getElementById("cajaTexto").value;
-    fetch("http://www.omdbapi.com/?apikey=d535906&s="+pelicaBusqueda, {method: "GET"})
+    peliculaBusqueda = document.getElementById("cajaTexto").value;
+    fetch("http://www.omdbapi.com/?apikey=d535906&s=" + peliculaBusqueda, {method: "GET"})
     .then((res) => res.json())
     .then((datosRecibidos) => {
         document.getElementById("numeroResultados").innerHTML = "Se han encontrado " + datosRecibidos.totalResults;
-
         let milista = document.getElementById("lista");
         milista.innerHTML = "";
         console.log(datosRecibidos);
-        for (pelicula of datosRecibidos.Search) {
+        for (let pelicula of datosRecibidos.Search) { 
             let li = document.createElement("li");
-            li.innerHTML = pelicula.Title +" - "+ pelicula.Year; 
+            li.innerHTML = pelicula.Title + " - " + pelicula.Year; 
+            li.addEventListener("click", () => detalles(pelicula.imdbID)); 
             milista.appendChild(li);
 
             let img = document.createElement("img");
             img.src = pelicula.Poster;
+            img.addEventListener("click", () => detalles(pelicula.imdbID)); 
             milista.appendChild(img);
         }
-        //document.getElementById("titulo").innerHTML = "He recibido " + datosRecibidos.usuarios.length + " usuarios";
-        console.log(datosRecibidos)
+
+        console.log(datosRecibidos);
     })
-    .catch((err) => console.error ("error: ", err));
+    .catch((err) => console.error("error: ", err));
 }
 
 function cargarPaginas () {
-    fetch("http://www.omdbapi.com/?apikey=d535906&s="+pelicaBusqueda+"&page="+paginaBuscar, {method: "GET"})
+    fetch("http://www.omdbapi.com/?apikey=d535906&s=" + peliculaBusqueda + "&page=" + paginaBuscar, {method: "GET"})
     .then((res) => res.json())
     .then((datosRecibidos) => {
-        paginaBuscar++
+        paginaBuscar++; 
         let milista = document.getElementById("lista");
         console.log(datosRecibidos);
-        for (pelicula of datosRecibidos.Search) {
+
+        for (let pelicula of datosRecibidos.Search) { 
             let li = document.createElement("li");
-            li.innerHTML = pelicula.Title +" - "+ pelicula.Year; 
+            li.innerHTML = pelicula.Title + " - " + pelicula.Year; 
+            li.addEventListener("click", () => detalles(pelicula.imdbID)); 
             milista.appendChild(li);
 
             let img = document.createElement("img");
             img.src = pelicula.Poster;
+            img.addEventListener("click", () => detalles(pelicula.imdbID)); 
             milista.appendChild(img);
         }
-        console.log(datosRecibidos)
+
+        console.log(datosRecibidos);
     })
-    .catch((err) => console.error ("error: ", err));
+    .catch((err) => console.error("error: ", err));
+}
+
+function detalles(imdbID) {
+    fetch("http://www.omdbapi.com/?apikey=d535906&i="+imdbID, {method: "GET"})
+    .then((res) => res.json())
+    .then((datosPelicula) => {
+        let detallesDiv = document.getElementById("detalles");
+        detallesDiv.innerHTML = ""; //Limpia los detalles previos
+
+        let titulo = document.createElement("h2");
+        titulo.textContent = datosPelicula.Title;
+        detallesDiv.appendChild(titulo);
+
+        let anio = document.createElement("p");
+        anio.textContent = "Year: " + datosPelicula.Year;
+        detallesDiv.appendChild(anio);
+
+        let director = document.createElement("p");
+        director.textContent = "Director: " + datosPelicula.Director;
+        detallesDiv.appendChild(director);
+
+        let actores = document.createElement("p");
+        actores.textContent = "Actors: " + datosPelicula.Actors;
+        detallesDiv.appendChild(actores);
+
+        let sinopsis = document.createElement("p");
+        sinopsis.textContent = "Synopsis: " + datosPelicula.Plot;
+        detallesDiv.appendChild(sinopsis);
+
+        let poster = document.createElement("img");
+        poster.src = datosPelicula.Poster;
+        detallesDiv.appendChild(poster);
+    })
+    .catch((err) => console.error("error: ", err));
 }
