c6ca17fce684722dd1267435aadf4dbfe1bbfded
Daniel Trivino <danieltrivinomarin@gmail.com>
2025-01-23 08:33:12 +0100

    Proyecto vue terminado y coultando credenciales

--- DWEC/VUE/Proyecto Vue/Proyecto Vue/src/firebase.js
+++ DWEC/VUE/Proyecto Vue/Proyecto Vue/src/firebase.js
@@ -1,16 +1,9 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, collection } from 'firebase/firestore'
+import { conexion } from './firebaseAPIKey'
 // ... other firebase imports
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA9YdcAFGg879B5LkEiMLNIJCuZq6qvObk",
-  authDomain: "proyecto-recordatorio-7f2cb.firebaseapp.com",
-  projectId: "proyecto-recordatorio-7f2cb",
-  storageBucket: "proyecto-recordatorio-7f2cb.firebasestorage.app",
-  messagingSenderId: "985418712199",
-  appId: "1:985418712199:web:c82635b07912a066baa422",
-  measurementId: "G-TQDQBKJSX0"
-};
+const firebaseConfig = initializeApp(conexion);
 
 export const firebaseApp = initializeApp(firebaseConfig)
 
